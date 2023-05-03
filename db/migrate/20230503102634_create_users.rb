de7763632573e088b1a599765e7ea49ccfc8d589
CHIKU BORAH <chiku.borah@w3villa.com>
2023-05-03 16:25:52 +0530

    Adding Space

--- db/migrate/20230503102634_create_users.rb
+++ db/migrate/20230503102634_create_users.rb
@@ -3,8 +3,8 @@ class CreateUsers < ActiveRecord::Migration[7.0]
     create_table :users do |t|
          t.string :username
          t.string :email
-         
+
       t.timestamps
-    end
+    end 
   end
 end
