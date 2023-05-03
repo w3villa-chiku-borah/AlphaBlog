ecb59388c75c234b47a4d96395e307ff546858b6
CHIKU BORAH <chiku.borah@w3villa.com>
2023-05-03 15:43:07 +0530

    adding controller create,update,,styling for every page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,68 @@
+
 class ArticlesController < ApplicationController
+  
+  before_action  :set_article, only:[:show, :edit, :update, :destroy]
+
+
     def show
-        @article = Article.find(params[:id])
+     
+    
     end
     def index
         @articles = Article.all
     end
+
+    def edit
+    end
+
+    def new
+        
+     @article = Article.new
+
+    end
+
+
+    def create
+        @article = Article.new(article_params)
+
+     if @article.save
+        flash[:notice] = "Article was created successfully"
+     #to redirect show page
+     redirect_to article_path(@article)
+     
+          #same efffect
+          #redirect_to @article
+          
+    else
+
+        return render(:new, status: :unprocessable_entity) unless @article.save      
+    end
+     
+    end
+    def update
+        if @article.update(article_params)
+            flash[:notice] = "Article was update successfully"
+            redirect_to article_path(@article)
+        else
+
+            return render(:edit, status: :unprocessable_entity) unless @article.save      
+        
+        end
+        end
+
+        def destroy
+            @article.destroy 
+            redirect_to articles_path
+        end
+
+private
+
+def set_article
+    @article = Article.find(params[:id])
+end
+
+def article_params
+  params.require(:article).permit(:title, :description)
+end
+
 end
---
+++ app/helpers/pages_helper.rb
@@ -0,0 +1,6 @@
+module PagesHelper
+    def home
+    end
+    def about
+    end
+end
